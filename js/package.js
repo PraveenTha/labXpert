fcd77da80e48af0e31b72c594bb7484559f6d372
PraveenTha <praveentha8@gmail.com>
2026-03-07 19:17:45 +0530

    update packages and Sampoorna Wellness Profile

--- js/package.js
+++ js/package.js
@@ -53,57 +53,99 @@ const packages = [
       { name: "Urine R/M", price: "₹100", parameters: "20 Parameters" }
     ]
   },
-
-  
-   {
+{
   name: "Sampoorna Wellness Profile",
   parameters: "120+ Parameters",
   price: "₹4999",
   oldPrice: "₹12000",
   tests: [
-    { name: "Blood Toxic Elements", parameters: "21 Parameters" },
-    { name: "Complete Blood Count (CBC)" },
-    { name: "ESR (Erythrocyte Sedimentation Rate)" },
-    { name: "Glycated Hemoglobin (HbA1c)" },
-    { name: "Thyroid Profile (T3, T4, TSH)" },
-    { name: "Iron Profile" },
-    { name: "Ferritin" },
-    { name: "Liver Function Test (LFT)" },
-    { name: "Lipid Profile" },
-    { name: "Kidney Function Test (KFT)" },
-    { name: "Glomerular Filtration Rate (GFR)" },
-    { name: "Lipoprotein (a)" },
-    { name: "Apolipoprotein B/A1" },
-    { name: "hs-CRP (High Sensitivity C-Reactive Protein)" },
-    { name: "Homocysteine" },
-    { name: "Fructosamine" },
-    { name: "Ketone" },
-    { name: "Insulin (Fasting)" },
-    { name: "Vitamin B12" },
-    { name: "Folic Acid" },
-    { name: "Testosterone - Total" },
-    { name: "Cystatin-C" },
-    { name: "Zinc" },
-    { name: "Copper" },
-    { name: "Amylase" },
-    { name: "Lipase" },
-    { name: "Anti-CCP" },
-    { name: "ANA (Antinuclear Antibody)" },
-    { name: "Vitamin K" },
-    { name: "Vitamin E" },
-    { name: "Vitamin B7 (Biotin)" },
-    { name: "Vitamin B6 (Pyridoxine)" },
-    { name: "Vitamin B5 (Pantothenic Acid)" },
-    { name: "Vitamin B3 (Niacin)" },
-    { name: "Vitamin B2 (Riboflavin)" },
-    { name: "Vitamin B1 (Thiamine)" },
-    { name: "Vitamin A" },
-    { name: "Vitamin D2, D3 (Total Vitamin D)" },
-    { name: "RA Quantitative" },
-    { name: "ASO Titre" },
-    { name: "Microalbumin Screen" },
-    { name: "Urine Creatinine" },
-    { name: "UACR Screening (Urine Albumin Creatinine Ratio)" }
+
+    { name: "Blood Toxic Elements", price: "₹2500", parameters: "21 Parameters" },
+
+    { name: "Complete Blood Count (CBC)", price: "₹390", parameters: "23 Parameters" },
+
+    { name: "ESR (Erythrocyte Sedimentation Rate)", price: "₹200", parameters: "1 Parameter" },
+
+    { name: "Glycated Hemoglobin (HbA1c)", price: "₹390", parameters: "3 Parameters" },
+
+    { name: "Thyroid Profile (T3, T4, TSH)", price: "₹290", parameters: "3 Parameters" },
+
+    { name: "Iron Profile", price: "₹550", parameters: "3 Parameters" },
+
+    { name: "Ferritin", price: "₹450", parameters: "1 Parameter" },
+
+    { name: "Liver Function Test (LFT)", price: "₹350", parameters: "11 Parameters" },
+
+    { name: "Lipid Profile", price: "₹290", parameters: "9 Parameters" },
+
+    { name: "Kidney Function Test (KFT)", price: "₹350", parameters: "11 Parameters" },
+
+    { name: "Glomerular Filtration Rate (GFR)", price: "₹300", parameters: "1 Parameter" },
+
+    { name: "Lipoprotein (a)", price: "₹800", parameters: "1 Parameter" },
+
+    { name: "Apolipoprotein B/A1", price: "₹750", parameters: "2 Parameters" },
+
+    { name: "hs-CRP (High Sensitivity C-Reactive Protein)", price: "₹600", parameters: "1 Parameter" },
+
+    { name: "Homocysteine", price: "₹850", parameters: "1 Parameter" },
+
+    { name: "Fructosamine", price: "₹600", parameters: "1 Parameter" },
+
+    { name: "Ketone", price: "₹200", parameters: "1 Parameter" },
+
+    { name: "Insulin (Fasting)", price: "₹700", parameters: "1 Parameter" },
+
+    { name: "Vitamin B12", price: "₹650", parameters: "1 Parameter" },
+
+    { name: "Folic Acid", price: "₹650", parameters: "1 Parameter" },
+
+    { name: "Testosterone - Total", price: "₹700", parameters: "1 Parameter" },
+
+    { name: "Cystatin-C", price: "₹900", parameters: "1 Parameter" },
+
+    { name: "Zinc", price: "₹800", parameters: "1 Parameter" },
+
+    { name: "Copper", price: "₹800", parameters: "1 Parameter" },
+
+    { name: "Amylase", price: "₹350", parameters: "1 Parameter" },
+
+    { name: "Lipase", price: "₹350", parameters: "1 Parameter" },
+
+    { name: "Anti-CCP", price: "₹1200", parameters: "1 Parameter" },
+
+    { name: "ANA (Antinuclear Antibody)", price: "₹1100", parameters: "1 Parameter" },
+
+    { name: "Vitamin K", price: "₹1200", parameters: "1 Parameter" },
+
+    { name: "Vitamin E", price: "₹1200", parameters: "1 Parameter" },
+
+    { name: "Vitamin B7 (Biotin)", price: "₹900", parameters: "1 Parameter" },
+
+    { name: "Vitamin B6 (Pyridoxine)", price: "₹850", parameters: "1 Parameter" },
+
+    { name: "Vitamin B5 (Pantothenic Acid)", price: "₹850", parameters: "1 Parameter" },
+
+    { name: "Vitamin B3 (Niacin)", price: "₹850", parameters: "1 Parameter" },
+
+    { name: "Vitamin B2 (Riboflavin)", price: "₹850", parameters: "1 Parameter" },
+
+    { name: "Vitamin B1 (Thiamine)", price: "₹850", parameters: "1 Parameter" },
+
+    { name: "Vitamin A", price: "₹900", parameters: "1 Parameter" },
+
+    { name: "Vitamin D2, D3 (Total Vitamin D)", price: "₹750", parameters: "1 Parameter" },
+
+    { name: "RA Quantitative", price: "₹450", parameters: "1 Parameter" },
+
+    { name: "ASO Titre", price: "₹400", parameters: "1 Parameter" },
+
+    { name: "Microalbumin Screen", price: "₹300", parameters: "1 Parameter" },
+
+    { name: "Urine Creatinine", price: "₹200", parameters: "1 Parameter" },
+
+    { name: "UACR Screening (Urine Albumin Creatinine Ratio)", price: "₹450", parameters: "2 Parameters" }
+
   ]
 }
 ];
